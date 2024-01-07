0593d6aebab34f4f20400bb6024156c3a2570185
Guillermo Rodas <glrodasz@gmail.com>
2024-01-07 16:35:21 +0100

    Little changes

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+import { buildTopics } from "./buildTopics.js";
+
 // Model configuration
 export const MODEL_TOKEN_LIMIT = 16385;
 export const SAFETY_MARGIN = 200;
@@ -5,12 +7,21 @@ export const GTP_MODEL = "gpt-3.5-turbo-1106";
 
 // Text processing
 export const SENTENCE_END_THRESHOLD_PERCENTAGE = 80;
-export const TOPICS = [
-  "technology",
-  "programming",
+export const TOPICS = buildTopics([
+  "tech",
   "javascript",
   "react",
-  "design",
   "soft skills",
-  "personal development",
-];
+  "node",
+  "design systems",
+  "css",
+  "html",
+  "career",
+  "sotrybook",
+  "testing",
+  "security",
+  "ideas",
+  "development",
+  "programming",
+  "inspiration"
+]);
--- promptBuilder.js
+++ promptBuilder.js
@@ -1,4 +1,3 @@
-import { buildTopics } from "./buildTopics.js";
 import { TOPICS } from "./config.js";
 
 
@@ -8,17 +7,15 @@ export function buildMessages(text) {
       role: "system",
       content: `Extract meaningful and engaging dialogues from a given text source. The source could be from a podcast, a YouTube video, or a recorded stream.
 
-      The key task is to identify and extract dialogues that are relevant, engaging, and meaninful on topics such as ${buildTopics(
-        TOPICS
-      )}.
+      The key task is to identify and extract dialogues that are relevant, engaging, and meaninful on topics such as ${TOPICS}.
 
       Requirements:
       - Each extracted dialogue should be at least 200 words but not more than 400 words, excluding filler words, greetings, alerts, and other non-dialogue audio.
-      - Ensure that the dialogues are suitable for a 90-second video clip.
+      - Ensure that the dialogues are suitable for a 60-second video clip.
       - Maintain the integrity of the original dialogues. Do not modify, alter the structure, summarize, or create new content based on the original text. Keep the dialogues exactly as they were in the source.
+      -  Ensure the dialogues are complete thoughts or discussions that can stand alone for viewers who haven't seen the original video.      
       - The output should be formatted in JSON array, but as plain text.Each object should have two properties: 'title' and 'dialogue', both in the original language of the source.
       -  If no relevant dialogues are found, please return an empty array, avoid return an apology or comment.
-      -  If a dialogue content is less than 200 words or more than 400 words please discard it, avoid return an apology or comment.
       - Avoid including any meta-commentary about the text or its language. directly focus on extracting the dialogues.
       - Don't use markdown for the output response, just plain text.`,
     },
